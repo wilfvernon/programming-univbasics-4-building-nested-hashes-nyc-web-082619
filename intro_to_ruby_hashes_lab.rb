ade34b15ddf06c0b7968b54185684bd345412f36
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 19:43:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,10 +24,16 @@ def monopoly_with_fourth_tier
   {railroads: {
   	  pieces: 4, 
   	  rent_in_dollars: {
-  	    one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200
+  	    one_piece_owned: 25, 
+  	    two_pieces_owned: 50, 
+  	    three_pieces_owned: 100, 
+  	    four_pieces_owned: 200
   	  },
   	  names: {
-  	    reading_railroad: {"mortgage_value" => "$100"}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}
+  	    reading_railroad: {"mortgage_value" => "$100"},
+  	    pennsylvania_railroad: {"mortgage_value" => "$200"},
+  	    b_and_o_railroad: {}, 
+  	    shortline_railroad: {}
   	  }
   	}}
 end
